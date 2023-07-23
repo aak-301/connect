df92c1d2d1e15281fa25c10fbdf1a222536afeb5
aakash-301 <aakashshivanshu5@gmail.com>
2023-07-23 14:44:31 +0530

    added login flow on the server

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/user";
+import User from "../models/user.js";
 
 /* Register User */
 
@@ -35,6 +35,26 @@ export const register = async (req, res) => {
 
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGGING IN*/
+export const login = async (req, res) => {
+  console.log("=========");
+  console.log(req.body);
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
