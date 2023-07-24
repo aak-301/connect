399c7a1a0431b6bcb786f6b49a7153188facc480
aakash-301 <aakashshivanshu5@gmail.com>
2023-07-24 10:26:49 +0530

    added some dummy data in app

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from './models/user.js';
+import Post from "./models/posts.js";
+import { users,posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -59,6 +62,9 @@ mongoose
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server Port: ${PORT}`);
+      /* Add Data One Time */
+      // User.insertMany(users);
+      // Post.insertMany(posts);
     });
   })
   .catch((e) => console.log(`${e} did not connect`));
